adf635a8a10ea41f06fb607c1a843e02ec1f2a16
ojy0533 <ojy0533@naver.com>
2023-12-12 17:38:07 +0900

    comment create

---
+++ backend/Routes/comment.ts
@@ -0,0 +1,75 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+import { verifyToken } from "./auth";
+
+const router = express.Router();
+
+const client = new PrismaClient();
+
+const select = {
+  id: true,
+  createdAt: true,
+  content: true,
+  userId: true,
+  user: {
+    select: {
+      account:true,
+    },
+  },
+};
+
+//댓글 생성
+router.post('/', verifyToken, async(req:any, res) => {
+  try{
+    const { content, postId }  = req.body; // 댓글은 글의 내용, 글 제목 가져온다
+    const { user } = req;
+
+    if(
+      !content ||
+      content.trim().length === 0
+    ){
+      return res.status(400).json({
+        message : "Not Exist Data",
+      });
+    }
+    if(
+      !postId ||
+      isNaN(+postId)
+    ){
+      return res.status(400).json({
+        message: "Post Id is not a Number",
+      });
+    }
+    // db 상에 postId가 존재하는지를 먼저 체크한다.
+    const post = await client.post.findUnique({
+      where : { 
+        id: +postId,
+      },
+    });
+
+    if(!post) { 
+      return res.status(400).json({
+        message: "Not Exist Post",
+      });
+    }
+    const comment = await client.comment.create({
+      data : {
+        content,
+        userId : user.id,
+        postId : post.id,
+      },
+      select,
+    });
+
+    return res.json(comment);
+    
+  }catch(error){
+    console.error(error);
+
+    return res.status(500).json({
+      message : "Server Error",
+    });
+  }
+});
+
+export default router;
