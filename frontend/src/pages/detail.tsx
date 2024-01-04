421a875d7a04b8968c484dd1bd90226bbdde467a
ojy0533 <ojy0533@naver.com>
2024-01-04 15:04:10 +0900

    page detail

--- frontend/src/pages/detail.tsx
+++ frontend/src/pages/detail.tsx
@@ -1,13 +1,13 @@
 import { FC, useEffect, useState } from "react";
 import { useMe } from "../hooks";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import { formatDistanceToNow } from "date-fns";
 import { ko } from "date-fns/locale";
 import Comment from "../components/Comment";
 import axios from "axios";
 import { IPost } from "./main";
-import { FiArrowLeft } from "react-icons/fi";
+import { FiArrowLeft, FiX , FiTrash2 } from "react-icons/fi";
 import { GrEdit } from "react-icons/gr";
 
 const Detail: FC = () => {
@@ -15,11 +15,13 @@ const Detail: FC = () => {
   const[editToggle, setEditToggle] = useState<boolean>(false);
   const[title, setTitle] = useState<string>("");
   const[content, setContent] = useState<string>("");
+  const[isOpen, setIsOpen] = useState<boolean>(false); // modal 창이 열려있는지를 판별
 
   const {account, getMe} = useMe();
  
   const { postId } = useParams();
 
+  const navigate = useNavigate();
 
   const getPost = async() => {
     try{
@@ -62,6 +64,25 @@ const Detail: FC = () => {
     }
   };
 
+  const onClickDelete = async () => {
+    try{
+      await axios.delete(`
+      ${process.env.REACT_APP_BACK_URL}/post/${postId}`,
+        {
+          headers:{
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      
+      navigate("/");
+    }catch(error){
+      console.error(error);
+    }
+  };
+
+
   useEffect(() => {
     getMe();
     getPost();
@@ -84,18 +105,26 @@ const Detail: FC = () => {
               </button>
             )}
             {account === post.user.account && (
-              <button
-                className="flex items-center"
-                onClick={()=> setEditToggle(!editToggle)}
-              >
-                {editToggle ? (
-                  "Cancel"
-                ) : (
-                  <>
-                    <GrEdit size={22}/>Edit
-                  </>
-                )}
-              </button>
+              <>  
+                <button
+                  className="flex items-center"
+                  onClick={()=> setEditToggle(!editToggle)}
+                >
+                  {editToggle ? (
+                    "Cancel"
+                  ) : (
+                    <>
+                      <GrEdit size={22}/>Edit
+                    </>
+                  )}
+                </button>
+                <button
+                  className="flex items-center"
+                  onClick={() =>setIsOpen(true)}
+                >
+                  <FiTrash2 size={22}/>Delete   
+                </button>
+              </>
             )}
           </div>
         </div>
@@ -138,6 +167,23 @@ const Detail: FC = () => {
       </div> ) : (<div>Loading...</div>)}
          <Comment postId={+postId!} />     
       </main>
+      {isOpen && ( 
+        <div className="fixed w-full h-full bg white top-0 left-0 bg-opacity-50 flex justify-center items-center">
+          <div className="text-xl p-8">
+            <div className="text-right">
+              <button onClick={() => setIsOpen(false)}>
+                <FiX/>
+              </button>
+            </div>
+            <h1 className="mt-8">정말 삭제하시겠습니까?</h1>
+            <div className="mt-8 text-center mt-4 flex justify-center">
+              <button className="flex items-center justify-center" onClick={onClickDelete}>
+                <FiTrash2 size={22}/> Delete  
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   ); 
 };
